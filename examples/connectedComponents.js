53759965e215eed6e2b6e365d2f88bfbbcc1c651
Yomguithereal <guillaumeplique@gmail.com>
2016-02-04 14:40:27 +0100

    Adding connected components example

---
+++ examples/connectedComponents.js
@@ -0,0 +1,63 @@
+// Using solution n°1 from issue #8
+function connectedComponents(graph) {
+  const visitedNodes = new Set(),
+        components = [];
+
+  graph.nodes.forEach(node => {
+
+    if (visitedNodes.has(node))
+      return;
+
+    const component = new Graph();
+
+    visitedNodes.add(node);
+    component.add(node);
+
+    const walk = neighbour => {
+      if (visitedNodes.has(neighbour))
+        return;
+
+      visitedNodes.add(neighbour);
+      component.add(neighbour);
+      graph.neighbours(neighbour).forEach(walk);
+    };
+
+    graph.neighbours(node).forEach(walk);
+
+    components.push(component);
+  });
+
+  return components;
+}
+
+// Using solution n°3 from issue #8
+function connectedComponents(graph) {
+  const visitedNodes = new Set(),
+        components = [];
+
+  graph.nodes.forEach(node => {
+
+    if (visitedNodes.has(node))
+      return;
+
+    const component = new Graph();
+
+    visitedNodes.add(node);
+    component.add(node);
+
+    const walk = neighbour => {
+      if (visitedNodes.has(neighbour))
+        return;
+
+      visitedNodes.add(neighbour);
+      component.add(neighbour);
+      neighbour.neighbours().forEach(walk);
+    };
+
+    node.neighbours().forEach(walk);
+
+    components.push(component);
+  });
+
+  return components;
+}
