1e49d4d83e9c1295d02df029e16dbb03a0db3ded
Yomguithereal <guillaumeplique@gmail.com>
2016-08-08 21:47:02 +0200

    Better search field

---
+++ docs/js/search.js
@@ -0,0 +1,57 @@
+require([
+  'gitbook',
+  'jquery'
+], function(gitbook, $) {
+  var INPUT = '';
+  var SELECTOR = '.chapter > a';
+  var HIDDEN = {};
+
+  // Adding the search input
+  function addSearchInput() {
+    $('.book-summary').prepend([
+      '<div id="book-search-input" role="search">',
+        '<input class="custom-input" type="text" placeholder="Search..." value="' + INPUT + '" />',
+      '</div>'
+    ].join('\n'));
+
+    $(SELECTOR).each(function() {
+      if (HIDDEN[$(this).parent().attr('data-level')])
+        $(this).hide();
+    });
+
+    $('#book-search-input > input').on('input', function() {
+      INPUT = ($(this).val() || '')
+        .trim()
+        .toLowerCase();
+
+      HIDDEN = {};
+
+      $(SELECTOR).show();
+
+      if (INPUT.length < 3)
+        return;
+
+      $(SELECTOR).each(function() {
+        var text = ($(this).text() || '')
+          .trim()
+          .toLowerCase();
+
+        if (!~text.indexOf(INPUT)) {
+          $(this).hide();
+          HIDDEN[$(this).parent().attr('data-level')] = true;
+        }
+        else {
+          var $ancestors = $(this).parents('.chapter').find('> a');
+
+          $ancestors.each(function() {
+            delete HIDDEN[$(this).parent().attr('data-level')];
+          });
+
+          $ancestors.show();
+        }
+      });
+    });
+  }
+
+  gitbook.events.on('page.change', addSearchInput);
+});
