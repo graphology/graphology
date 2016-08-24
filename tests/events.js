6f7114c6882772260667aa362dc9fe4d2da8ee80
Yomguithereal <guillaumeplique@gmail.com>
2016-08-24 18:42:04 +0200

    Starting events

---
+++ tests/events.js
@@ -0,0 +1,17 @@
+/**
+ * Graphology Events Specs
+ * ========================
+ *
+ * Testing the graph's events.
+ */
+import assert from 'assert';
+
+export default function events(Graph) {
+  return {
+    'cleared': {
+      'it should fire when the graph is cleared.': function() {
+
+      }
+    }
+  };
+}
