f96bc2419529d74baa9ddfe221129c839cee046e
Yomguithereal <guillaumeplique@gmail.com>
2020-02-05 21:38:46 +0100

    More tests

--- test-types.ts
+++ test-types.ts
@@ -1,3 +1,13 @@
 import Graph, {UndirectedGraph} from './src';
 
 const graph: Graph = new Graph({type: 'directed'});
+
+graph.addNode('one', {label: 'Hello'});
+
+graph.forEachNode((node, attr) => {
+  console.log(node, attr);
+});
+
+const undirectedGraph: UndirectedGraph = new UndirectedGraph();
+
+undirectedGraph.clear();
