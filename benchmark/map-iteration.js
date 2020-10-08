7f62e93a761f13a537cf860351bbb897a890f7e8
Yomguithereal <guillaumeplique@gmail.com>
2020-10-08 19:15:57 +0200

    Adding map iteration benchmark

---
+++ benchmark/map-iteration.js
@@ -0,0 +1,52 @@
+const assert = require('assert');
+const randomString = require('pandemonium/random-string');
+
+const key = () => randomString(4, 87);
+
+const N = 5000000;
+const I = 100;
+
+let i;
+
+const map = new Map();
+
+while (map.size < N)
+  map.set(key(), Math.random());
+
+console.log('Finished preparations...');
+console.log(map.size);
+
+let s = '';
+let c = 0;
+console.time('forEach');
+for (i = 0; i < I; i++)
+  map.forEach(value => {
+    s = value;
+    c++;
+  });
+console.timeEnd('forEach');
+assert.strictEqual(c, N * I);
+
+c = 0;
+console.time('for of');
+for (i = 0; i < I; i++) {
+  for (const value of map) {
+    s = value;
+    c++;
+  }
+}
+console.timeEnd('for of');
+assert.strictEqual(c, N * I);
+
+c = 0;
+console.time('next');
+let step, it;
+for (i = 0; i < I; i++) {
+  it = map.values();
+  while ((step = it.next(), step.done !== true)) {
+    s = step.value;
+    c++;
+  }
+}
+console.timeEnd('next');
+assert.strictEqual(c, N * I);
