7bba4b0e4fcb63d111e42b33b4b08cb101f79014
Yomguithereal <guillaumeplique@gmail.com>
2020-10-10 21:53:53 +0200

    Bench for string casting

---
+++ benchmark/string-casting.js
@@ -0,0 +1,31 @@
+var randomString = require('pandemonium/random-string');
+
+var N = 1000000000;
+var S = 5000;
+
+var strings = Array.from(new Array(S), () => randomString(4, 100));
+
+let i, s;
+
+console.time('ninja cast');
+for (i = 0; i < N; i++)
+  s = '' + strings[i % S];
+console.timeEnd('ninja cast');
+
+console.time('toString');
+for (i = 0; i < N; i++)
+  s = strings[i % S].toString();
+console.timeEnd('toString');
+
+console.time('condition');
+for (i = 0; i < N; i++) {
+  s = strings[i % S];
+  s = typeof s === 'string' ? s : ('' + s);
+}
+console.timeEnd('condition');
+
+console.time('constructor');
+for (i = 0; i < N; i++) {
+  s = String(strings[i % S]);
+}
+console.timeEnd('constructor');
