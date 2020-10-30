0d6ac6b53fb3620a7daed1c691c0819aa03bdfa9
Yomguithereal <guillaumeplique@gmail.com>
2020-10-30 20:32:47 +0100

    Upgrading types

--- test-types.ts
+++ test-types.ts
@@ -1,12 +1,24 @@
 import Graph, {UndirectedGraph} from './src/endpoint.esm';
 
-const graph: Graph = new Graph({type: 'directed'});
+interface NodeAttributes {
+  label: string
+};
+
+const graph: Graph<NodeAttributes> = new Graph({type: 'directed'});
 
 graph.addNode('one', {label: 'Hello'});
 
 graph.forEachNode((node, attr) => {
   console.log(node, attr);
 });
+
+function listener({key, attributes}: {key: string, attributes: NodeAttributes}) {
+  console.log(key);
+  console.log(attributes.label);
+}
+
+graph.on('nodeAdded', listener);
+graph.off('nodeAdded', listener);
 
 const undirectedGraph: UndirectedGraph = new UndirectedGraph();
 
