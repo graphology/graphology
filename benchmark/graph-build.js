3e9b8a026c32287d945c8edf741096e2f6985670
Yomguithereal <guillaumeplique@gmail.com>
2020-11-04 08:45:53 +0100

    Bench on a complete graph

--- benchmark/graph-build.js
+++ benchmark/graph-build.js
@@ -4,11 +4,30 @@ const randomString = require('pandemonium/random-string');
 
 const Graph = require('../src/endpoint.cjs').default;
 
-const E = 1000000;
+const N = 1500;
 
 const g = new Graph();
+const keys = new Array(N);
+const keySet = new Set();
+
+let i, j, k;
+
+for (i = 0; i < N; i++) {
+  do {
+    k = randomString(4, 50);
+  } while (keySet.has(k));
+  keySet.add(k);
+  keys[i] = k;
+  g.addNode(k);
+}
 
 console.time('build');
-while (g.size < E)
-  g.mergeEdge(randomString(4, 50), randomString(4, 50));
+for (i = 0; i < N; i++) {
+  for (j = 0; j < N; j++) {
+    if (i === j)
+      continue;
+
+    g.addEdge(keys[i], keys[j]);
+  }
+}
 console.timeEnd('build');
