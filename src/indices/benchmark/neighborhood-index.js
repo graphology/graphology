e1447ad328cbb12f12417cddca3a989f532ee98d
Yomguithereal <guillaumeplique@gmail.com>
2021-10-31 23:19:55 +0100

    Making sure neighborhood indexing is useful

--- src/indices/benchmark/neighborhood-index.js
+++ src/indices/benchmark/neighborhood-index.js
@@ -11,5 +11,19 @@ console.log(graph.order, graph.size);
 console.log();
 
 console.time('OutboundNeighborhoodIndex');
-new OutboundNeighborhoodIndex(graph);
+const index = new OutboundNeighborhoodIndex(graph);
 console.timeEnd('OutboundNeighborhoodIndex');
+
+console.time('Iteration');
+let i = 0;
+let n = 0;
+let j = 0;
+
+for (i = 0; i < index.nodes.length; i++) {
+  for (n = index.starts[i]; n < index.starts[i + 1]; n++) {
+    j++;
+  }
+}
+console.timeEnd('Iteration');
+
+console.log(j, graph.directedSize + graph.undirectedSize * 2);
