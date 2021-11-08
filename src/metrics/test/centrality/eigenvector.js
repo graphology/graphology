844d746a2e556303917e3530706522f376152c7b
Yomguithereal <guillaumeplique@gmail.com>
2021-11-08 11:12:54 +0100

    [metrics] adding more tests for eigenvector

--- src/metrics/test/centrality/eigenvector.js
+++ src/metrics/test/centrality/eigenvector.js
@@ -12,6 +12,37 @@ function deepApproximatelyEqual(t, o, precision) {
   for (var k in t) assert.approximately(t[k], o[k], precision);
 }
 
+function getDirectedGraph() {
+  var edges = [
+    [1, 2],
+    [1, 3],
+    [2, 4],
+    [3, 2],
+    [3, 5],
+    [4, 2],
+    [4, 5],
+    [4, 6],
+    [5, 6],
+    [5, 7],
+    [5, 8],
+    [6, 8],
+    [7, 1],
+    [7, 5],
+    [7, 8],
+    [8, 6],
+    [8, 7]
+  ];
+
+  var graph = new Graph.DirectedGraph();
+
+  edges.forEach(function (pair) {
+    // NOTE: reversed
+    graph.mergeEdge(pair[1], pair[0], {weight: 2});
+  });
+
+  return graph;
+}
+
 describe('eigenvector centrality', function () {
   it('should throw if provided with something which is not a graph.', function () {
     assert.throws(function () {
@@ -36,6 +67,18 @@ describe('eigenvector centrality', function () {
     }, {});
 
     deepApproximatelyEqual(result, expected, 1e-7);
+
+    // Assign
+    eigenvectorCentrality.assign(graph);
+
+    deepApproximatelyEqual(
+      graph.reduceNodes(function (o, node, attr) {
+        o[node] = attr.eigenvectorCentrality;
+        return o;
+      }, {}),
+      expected,
+      1e-7
+    );
   });
 
   it('should return the correct results with a path graph.', function () {
@@ -50,5 +93,38 @@ describe('eigenvector centrality', function () {
       },
       1e-4
     );
+
+    graph.setEdgeAttribute(0, 1, 'weight', 5);
+
+    deepApproximatelyEqual(
+      eigenvectorCentrality(graph, {weighted: true}),
+      {
+        0: 0.6933,
+        1: 0.7071,
+        2: 0.1386
+      },
+      1e-4
+    );
+  });
+
+  it('should work with directed graphs.', function () {
+    var expected = {
+      1: 0.25368793,
+      2: 0.19576478,
+      3: 0.32817092,
+      4: 0.40430835,
+      5: 0.48199885,
+      6: 0.15724483,
+      7: 0.51346196,
+      8: 0.32475403
+    };
+
+    var graph = getDirectedGraph();
+
+    var unweightedResult = eigenvectorCentrality(graph);
+    var weightedResult = eigenvectorCentrality(graph, {weighted: true});
+
+    deepApproximatelyEqual(weightedResult, expected, 1e-5);
+    deepApproximatelyEqual(unweightedResult, expected, 1e-5);
   });
 });
