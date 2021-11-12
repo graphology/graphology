713b4b10751a8208fb7543fa06527345034a678b
Yomguithereal <guillaumeplique@gmail.com>
2021-11-12 17:26:25 +0100

    [bipartite] adding isBipartiteBy

--- src/bipartite/is-bipartite-by.js
+++ src/bipartite/is-bipartite-by.js
@@ -3,13 +3,36 @@
  * =============================
  *
  * Function returning whether the given graph is biparte according to some
- * partition criteria.
+ * partition criterion.
  */
 const isGraph = require('graphology-utils/is-graph');
 
-module.exports = function isBipartiteBy(graph) {
+module.exports = function isBipartiteBy(graph, getNodePartition) {
   if (!isGraph(graph))
     throw new Error(
       'graphology-bipartite/is-bipartite-by: the given graph is not a valid graphology instance.'
     );
+
+  if (typeof getNodePartition === 'string') {
+    const nodePartitionAttribute = getNodePartition;
+    getNodePartition = (_, attr) => attr[nodePartitionAttribute];
+  }
+
+  const seenPartitions = [];
+
+  return graph.everyNode((node, attr) => {
+    const p1 = getNodePartition(node, attr);
+
+    if (!seenPartitions.includes(p1)) {
+      seenPartitions.push(p1);
+
+      if (seenPartitions.length > 2) return false;
+    }
+
+    return graph.everyOutboundNeighbor(node, (neighbor, neighborAttr) => {
+      const p2 = getNodePartition(neighbor, neighborAttr);
+
+      return p1 !== p2;
+    });
+  });
 };
--- src/bipartite/test.js
+++ src/bipartite/test.js
@@ -2,4 +2,55 @@
  * Graphology Bipartite Unit Tests
  * ================================
  */
-describe('graphology-bipartite', function () {});
+const Graph = require('graphology');
+const assert = require('assert');
+const isBipartiteBy = require('./is-bipartite-by.js');
+
+function createBipartiteGraph() {
+  const graph = new Graph({type: 'undirected'});
+
+  graph.addNode('John', {partition: 'people'});
+  graph.addNode('Mary', {partition: 'people'});
+  graph.addNode('Susan', {partition: 'people'});
+
+  graph.addNode('red', {partition: 'color'});
+  graph.addNode('blue', {partition: 'color'});
+  graph.addNode('yellow', {partition: 'color'});
+  graph.addNode('orange', {partition: 'color'});
+
+  graph.addEdge('John', 'red');
+  graph.addEdge('John', 'yellow');
+  graph.addEdge('Mary', 'yellow');
+  graph.addEdge('Mary', 'blue');
+  graph.addEdge('Susan', 'blue');
+
+  return graph;
+}
+
+describe('graphology-bipartite', function () {
+  describe('isBipartiteBy', function () {
+    it('should throw if given an invalid graph.', function () {
+      assert.throws(function () {
+        isBipartiteBy(null);
+      });
+    });
+
+    it('should correctly return whether a graph is bipartite according to the given criterion.', function () {
+      const graph = createBipartiteGraph();
+
+      assert.strictEqual(isBipartiteBy(graph, 'partition'), true);
+      assert.strictEqual(
+        isBipartiteBy(graph, (_, a) => a.partition),
+        true
+      );
+      assert.strictEqual(
+        isBipartiteBy(graph, () => 'same'),
+        false
+      );
+      assert.strictEqual(
+        isBipartiteBy(graph, n => n),
+        false
+      );
+    });
+  });
+});
