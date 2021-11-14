092f33ed96034cc409560185b2668d287a4bf567
Yomguithereal <guillaumeplique@gmail.com>
2021-11-14 13:44:57 +0100

    [utils] starting to work on getters refactor

--- src/utils/getters.js
+++ src/utils/getters.js
@@ -4,17 +4,19 @@
  *
  * Function creating weight getters.
  */
+function coerceWeight(value) {
+  // Ensuring target value is a correct number
+  if (typeof value !== 'number' || isNaN(value)) return 1;
+
+  return value;
+}
+
 exports.createWeightGetter = function (name) {
   var weightGetter = function (attr) {
     // If no name was provided, it means we don't want a weighted computation
     if (!name) return 1;
 
-    var weight = attr[name];
-
-    // Ensuring target value is a correct number
-    if (typeof weight !== 'number' || isNaN(weight)) weight = 1;
-
-    return weight;
+    return coerceWeight(attr[name]);
   };
 
   return weightGetter;
