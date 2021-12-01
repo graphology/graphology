28b723f3d54675105cb695f4611034ac0bdf95df
Yomguithereal <guillaumeplique@gmail.com>
2021-12-01 16:10:03 +0100

    Drafting graphology-sparsification Related to #305

---
+++ src/sparsification/README.md
@@ -0,0 +1,13 @@
+# Graphology Sparsification
+
+Sparsification methods and helpers to be used with [`graphology`](https://graphology.github.io).
+
+## Installation
+
+```
+npm install graphology-sparsification
+```
+
+## Usage
+
+TODO...
---
+++ src/sparsification/test.js
@@ -0,0 +1,8 @@
+/**
+ * Graphology Sparsification Unit Tests
+ * =====================================
+ */
+// const Graph = require('graphology');
+// const assert = require('assert');
+
+describe('graphology-sparsification', function () {});
