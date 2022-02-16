5f3c4abbbdcc9dd2cfc7d1bcbaec9aa3cd823820
Yomguithereal <guillaumeplique@gmail.com>
2022-02-16 11:43:03 +0100

    Adding doubly-linked list unit test

--- src/graphology/tests/misc.js
+++ src/graphology/tests/misc.js
@@ -34,6 +34,43 @@ export default function misc(Graph) {
 
           assert.deepStrictEqual(graph.neighbors('A'), []);
           assert.deepStrictEqual(graph.neighbors('B'), []);
+        },
+
+      'exhaustive deletion use-cases should not break doubly-linked lists implementation of multigraph edge storage.':
+        function () {
+          const graph = new Graph({multi: true});
+          graph.mergeEdgeWithKey('1', 'A', 'B');
+          graph.mergeEdgeWithKey('2', 'A', 'B');
+          graph.mergeEdgeWithKey('3', 'A', 'B');
+          graph.mergeEdgeWithKey('4', 'A', 'B');
+
+          graph.dropEdge('1');
+          graph.dropEdge('2');
+          graph.dropEdge('3');
+          graph.dropEdge('4');
+
+          assert.strictEqual(graph.size, 0);
+          assert.strictEqual(graph.areNeighbors('A', 'B'), false);
+
+          graph.mergeEdgeWithKey('1', 'A', 'B');
+          graph.mergeEdgeWithKey('2', 'A', 'B');
+          graph.mergeEdgeWithKey('3', 'A', 'B');
+          graph.mergeEdgeWithKey('4', 'A', 'B');
+
+          assert.strictEqual(graph.size, 4);
+          assert.strictEqual(graph.areNeighbors('A', 'B'), true);
+
+          graph.dropEdge('2');
+          graph.dropEdge('3');
+
+          assert.strictEqual(graph.size, 2);
+          assert.strictEqual(graph.areNeighbors('A', 'B'), true);
+
+          graph.dropEdge('4');
+          graph.dropEdge('1');
+
+          assert.strictEqual(graph.size, 0);
+          assert.strictEqual(graph.areNeighbors('A', 'B'), false);
         }
     },
 
