046b444197b7c5b3c45ab0935f032f853e9af50c
Yomguithereal <guillaumeplique@gmail.com>
2024-02-19 09:42:13 +0100

    [canvas] Sidestepping canvas dep problems Related to #505

--- src/canvas/test/index.js
+++ src/canvas/test/index.js
@@ -3,28 +3,12 @@
  * =============================
  */
 var assert = require('assert');
-var Graph = require('graphology');
-var canvasApi = require('canvas');
-var lib = require('../');
-
-function createContext(width, height) {
-  var canvas = canvasApi.createCanvas(width || 2048, height || 2048);
-  var context = canvas.getContext('2d');
-
-  return context;
-}
+var DEFAULT_NODE_REDUCER = require('../defaults.js').DEFAULT_NODE_REDUCER;
 
 describe('graphology-canvas', function () {
   it('should throw if some node has no position.', function () {
-    var graph = new Graph();
-
-    graph.addNode('John', {x: 35, y: 76});
-    graph.addNode('Mary', {y: 45});
-
-    var context = createContext();
-
     assert.throws(function () {
-      lib.render(graph, context);
+      DEFAULT_NODE_REDUCER({}, 'John', {color: 'green'});
     }, /position/);
   });
 });
