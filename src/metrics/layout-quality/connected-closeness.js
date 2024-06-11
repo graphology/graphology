d65f8e0c362bd7c6528438bb3dec392d1a3d0b60
Mathieu Jacomy <Mathieu.Jacomy@gmail.com>
2024-06-11 15:08:45 +0200

    Basic frame for connected-closeness

---
+++ src/metrics/layout-quality/connected-closeness.js
@@ -0,0 +1,20 @@
+/**
+ * Graphology Layout Quality - Connected-closeness
+ * ============================================
+ *
+ * Function computing the layout quality metric named "connected-closeness",
+ * designed to provide a quantified statement about the mediation of the topology by the node placement.
+ *
+ * [Article]:
+ * Jacomy, M. (2023). Connected-closeness: A Visual Quantification of Distances in Network Layouts.
+ * Journal of Graph Algorithms and Applications, 27(5), 341-404.
+ * https://www.jgaa.info/index.php/jgaa/article/view/paper626
+ */
+var isGraph = require('graphology-utils/is-graph');
+
+module.exports = function edgeUniformity(graph) {
+  if (!isGraph(graph))
+    throw new Error(
+      'graphology-metrics/layout-quality/connected-closeness: given graph is not a valid graphology instance.'
+    );
+};
