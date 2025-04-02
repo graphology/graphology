e100dbe4f2a5efe406f5dff6f1d844b51e8de51d
Paul Girard <paul@ouestware.com>
2025-04-02 17:13:01 +0200

    [metrics] chi square forgot Thresholds in type declaration

--- src/metrics/edge/chi-square-g-square.d.ts
+++ src/metrics/edge/chi-square-g-square.d.ts
@@ -13,6 +13,16 @@ export type ChiGSquareMapping = {
   [edge: string]: {chiSquare: number | undefined; GSquare: number | undefined};
 };
 
+export interface Thresholds {
+  'pValue<0.5': number;
+  'pValue<0.1': number;
+  'pValue<0.05': number;
+  'pValue<0.025': number;
+  'pValue<0.01': number;
+  'pValue<0.005': number;
+  'pValue<0.001': number;
+}
+
 interface IChiGSquare {
   <
     NodeAttributes extends Attributes = Attributes,
@@ -29,6 +39,8 @@ interface IChiGSquare {
     graph: Graph<NodeAttributes, EdgeAttributes>,
     options?: ChiGSquareOptions<NodeAttributes, EdgeAttributes>
   ): void;
+
+  thresholds: Thresholds;
 }
 
 declare const chiSquareGSquare: IChiGSquare;
