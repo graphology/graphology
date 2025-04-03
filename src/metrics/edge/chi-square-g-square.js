9c890b5d04b0945881b629c9693f658415a70d0f
Paul Girard <paul@ouestware.com>
2025-04-03 11:42:21 +0200

    [metrics] separate chi-square and g-square in two metrics - added documentation - solved bug in types and exports

--- src/metrics/edge/chi-square-g-square.js
+++ src/metrics/edge/chi-square-g-square.js
@@ -18,13 +18,14 @@ var DEFAULT_WEIGHT_ATTRIBUTE = 'weight';
 /**
  * chiSquareGSquareMeasures
  * implementation copied from https://github.com/medialab/xan/blob/master/src/cmd/vocab.rs#L924-L1000
+ * @param {chiSquare|gSquare} measure       - either chiSquare or gSquare measure to compute
  * @param {number} sourceWeightedDegree 
  * @param {number} targetWeightedDegree 
  * @param {number} edgeWeight 
  * @param {number} sumAllEdgesWeights 
  * @returns Record<edge, {chiSquare:number, gSquare:number}>
  */
-function chiSquareGSquareMeasures(sourceWeightedDegree, targetWeightedDegree, edgeWeight, sumAllEdgesWeights){
+function _chiSquareGSquareMeasures(measure, sourceWeightedDegree, targetWeightedDegree, edgeWeight, sumAllEdgesWeights){
     
     if (sumAllEdgesWeights <= 0)
         throw new Error('sumAllEdgesWeights has to be >0')
@@ -50,53 +51,55 @@ function chiSquareGSquareMeasures(sourceWeightedDegree, targetWeightedDegree, ed
 
     if (edgeWeight < expected11){
         // under-represented token, measure will be biased, let's ignore the token
-        return {chiSquare: undefined, gSquare: undefined}
+        return undefined;
     }
 
     var expected12 = (sourceWeightedDegree * notNbTokensInDoc)  / sumAllEdgesWeights
     var expected21 = (targetWeightedDegree * notGf) / sumAllEdgesWeights
     var expected22 = (notGf * notNbTokensInDoc) / sumAllEdgesWeights
 
-    var chiSquare11 = Math.pow(observed11 - expected11, 2) / expected11
-    var chiSquare12 = Math.pow(observed12 - expected12, 2) / expected12
-    var chiSquare21 = Math.pow(observed21 - expected21, 2) / expected21
-    var chiSquare22 = Math.pow(observed22 - expected22, 2) / expected22
+    switch(measure){
+      case "gSquare":
+        var gSquare11 = observed11 * Math.log(observed11 / expected11)
+        var gSquare12 = observed12 === 0.0 ? 0.0 : observed12 * Math.log( (observed12 / expected12))
+        var gSquare21 = observed21 === 0.0 ? 0.0 : observed21 * Math.log( (observed21 / expected21))
+        // NOTE: in the case when observed_22 is negative, I am not entirely
+        // sure it is mathematically sound to clamp to 0. But since this case
+        // is mostly useless, I will allow it...
+        var gSquare22 = observed22 <= 0.0 ? 0.0 : observed22 * Math.log( (observed22 / expected22))
+        var gSquare = 2.0 * (gSquare11 + gSquare12 + gSquare21 + gSquare22) // 2.0 * is here to adjust g2 in the same scale as chi2
+        if (gSquare === Infinity)
+          gSquare = gSquare11
+        return gSquare;
+      default:
+      case "chiSquare":
+        var chiSquare11 = Math.pow(observed11 - expected11, 2) / expected11
+        var chiSquare12 = Math.pow(observed12 - expected12, 2) / expected12
+        var chiSquare21 = Math.pow(observed21 - expected21, 2) / expected21
+        var chiSquare22 = Math.pow(observed22 - expected22, 2) / expected22
 
-    var gSquare11 = observed11 * Math.log(observed11 / expected11)
-    var gSquare12 = observed12 === 0.0 ? 0.0 : observed12 * Math.log( (observed12 / expected12))
-    var gSquare21 = observed21 === 0.0 ? 0.0 : observed21 * Math.log( (observed21 / expected21))
+        var chiSquare = chiSquare11 + chiSquare12 + chiSquare21 + chiSquare22
+        //  Dealing with degenerate cases that happen when the number
+        //  of co-occurrences is very low, or when some item dominates
+        //  the distribution.
+        if (isNaN(chiSquare))
+          chiSquare = 0.0
 
-    // NOTE: in the case when observed_22 is negative, I am not entirely
-    // sure it is mathematically sound to clamp to 0. But since this case
-    // is mostly useless, I will allow it...
-    var gSquare22 = observed22 <= 0.0 ? 0.0 : observed22 * Math.log( (observed22 / expected22))
-
-    var chiSquare = chiSquare11 + chiSquare12 + chiSquare21 + chiSquare22
-    var gSquare = 2.0 * (gSquare11 + gSquare12 + gSquare21 + gSquare22) // 2.0 * is here to adjust g2 in the same scale as chi2
-
-    //  Dealing with degenerate cases that happen when the number
-    //  of co-occurrences is very low, or when some item dominates
-    //  the distribution.
-    if (isNaN(chiSquare))
-      chiSquare = 0.0
-
-    if (chiSquare === Infinity)
-        chiSquare = chiSquare11
-
-    if (gSquare === Infinity)
-        gSquare = gSquare11
-    
-    return {chiSquare:chiSquare, gSquare:gSquare}
+        if (chiSquare === Infinity)
+            chiSquare = chiSquare11
+        return chiSquare;
+    };
 }
 
 /**
  * Asbtract function to perform chi-square adn g-square tests measures.
  *
- * @param  {Graph}           graph         - A graphology instance.
- * @param  {string|function} getEdgeWeight - Name of edge weight attribute or getter function.
+ * @param {chiSquare|gSquare} measure       - either chiSquare or gSquare measure to compute
+ * @param  {Graph}            graph         - A graphology instance.
+ * @param  {string|function}  getEdgeWeight - Name of edge weight attribute or getter function.
  *
  */
-function abstractChiSquareGSquare(assign, graph, getEdgeWeight) {
+function abstractChiSquareGSquare(assign, measure, graph, getEdgeWeight) {
   if (!isGraph(graph))
     throw new Error(
       'graphology-metrics/chi-square: given graph is not a valid graphology instance.'
@@ -131,7 +134,7 @@ function abstractChiSquareGSquare(assign, graph, getEdgeWeight) {
       }); 
     });
 
-  var edgeChiSquareGSquareMeasures = {};
+  var edgeMeasures = {};
   graph.forEachAssymetricAdjacencyEntry(function (
       source,
       target,
@@ -144,25 +147,22 @@ function abstractChiSquareGSquare(assign, graph, getEdgeWeight) {
       var weight =  typeof getEdgeWeight === 'function'
       ? getEdgeWeight(edge, attr, source, target, sa, ta, undirected)
       : attr[getEdgeWeight]
-      var measures = chiSquareGSquareMeasures(
+      var result = _chiSquareGSquareMeasures(
+        measure,
         weightedDegrees[source],
         weightedDegrees[target],
         weight, 
         totalWeights)
-      edgeChiSquareGSquareMeasures[edge] = measures
-      
+      edgeMeasures[edge] = result
       if(assign) {
-        graph.setEdgeAttribute(edge, 'chiSquare', measures.chiSquare);
-        graph.setEdgeAttribute(edge, 'gSquare', measures.gSquare);
+        graph.setEdgeAttribute(edge, measure, result);
       }
   })
-
-  return edgeChiSquareGSquareMeasures
+  return edgeMeasures
 }
 
-var chiSquareGSquare = abstractChiSquareGSquare.bind(null, false);
-chiSquareGSquare.assign = abstractChiSquareGSquare.bind(null, true);
-chiSquareGSquare.thresholds =  {
+
+abstractChiSquareGSquare.thresholds =  {
   'pValue<0.5': 0.45, // lowest significance
   'pValue<0.1': 2.71, // very low significance
   'pValue<0.05': 3.84, // low significance
@@ -173,4 +173,4 @@ chiSquareGSquare.thresholds =  {
 };
 
 
-module.exports = chiSquareGSquare;
+module.exports = abstractChiSquareGSquare
--- src/metrics/edge/index.js
+++ src/metrics/edge/index.js
@@ -1,2 +1,4 @@
 exports.disparity = require('./disparity.js');
 exports.simmelianStrength = require('./simmelian-strength.js');
+exports.chiSquare = require('./chi-square.js');
+exports.gSquare = require('./g-square.js');
